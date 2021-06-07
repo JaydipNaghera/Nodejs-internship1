a58a5daa58ba2ab774ea94720bc624954c9d9289
JaydipNaghera <84767381+JaydipNaghera@users.noreply.github.com>
2021-06-07 19:14:46 +0530

    Delete day2 directory

--- day2/file1.js
+++
@@ -1,3 +0,0 @@
-//create moduale and export
-
-module.exports = "Welcome to file 1 code";
--- day2/file2.js
+++
@@ -1,7 +0,0 @@
-//load local module
-
-var mycontent = require('./file1.js');
-
-
-//Print Content 
-console.log(mycontent);
